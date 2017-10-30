ecf46c5d362b7b075127370859d90764becb162e
Miguel Marin Vermelho <miguelmarin95@hotmail.com>
2017-10-30 19:45:07 +0000

    added signup basic form logic, created user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,9 @@
+export class User{
+
+    //Make the fields a property of this class; firstName and lastName are optional so that the model can be reused for 
+    //login and signup.
+    constructor(public email : string, public password: string, public firstName? : string, public lastName? : string){
+        
+    }
+
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,3 +1,5 @@
+import { User } from './../models/user.model';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  signupForm : FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.signupForm= new FormGroup({
+      firstname: new FormControl('', Validators.required),
+      lastname: new FormControl('', Validators.required),
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("[a-zA-Z0-9]*@[a-zA-Z0-9]*.[a-z]{3}")
+       ]),
+      password: new FormControl('', Validators.required),
+      repeatPassword:  new FormControl('', Validators.required)
+      
+
+    })
   }
 
+
+  signup(){
+    
+    let firstname = this.signupForm.controls.firstname.value;
+    let lastname = this.signupForm.controls.lastname.value;
+    let email = this.signupForm.controls.email.value;
+    let password = this.signupForm.controls.password.value;
+    let passwordRepeat = this.signupForm.controls.passwordRepeat.value;
+
+
+    /*If passwords match user is allowed to sign up */
+    if(password === passwordRepeat){
+      //password needs hashing
+      let newUser = new User(email, password, firstname, lastname);
+      console.log(newUser);
+    }
+
+  }
 }

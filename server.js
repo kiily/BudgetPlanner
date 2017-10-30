96fe7c18be2193b88eab801dc528fec822da080d
Miguel Marin Vermelho <miguelmarin95@hotmail.com>
2017-10-30 23:57:36 +0000

    express api and node server setup, added cors, bodyparser, changed src to client, need to fix dist folder paths, created build command

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+//create the app
+const app = express();
+
+//API file for interaction with MongoDB
+var api = require('./server/routes/api.js')
+
+// Parsers - parses requests and makes them accessible with req.body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+
+/*Angular DIST output folder - this is the frontend build
+express.static is a built in middleware function to serve static files.
+ Pointing the express server to the dist folder as the place to look for the static files */
+ app.use(express.static(path.join(__dirname, 'dist')));
+ app.use('/', api);
+
+ //Send all other http requests to the Angular App
+ app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+ });
+
+ //Middleware for CORS
+ app.use(cors());
+
+ //Set port
+ const port = process.env.PORT || '3000';
+ app.set('port', port);
+ 
+ const server = http.createServer(app);
+ 
+ server.listen(port, () => console.log(`Running on localhost:${port}`));

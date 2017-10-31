5a2182ad7c9672e9603ebd2a52b104ac10114f9b
Miguel Marin Vermelho <miguelmarin95@hotmail.com>
2017-10-31 00:41:16 +0000

    fixed 3000 port issue, login child path added, login form now loads on the landing page directly

--- client/app/app.routes.ts
+++ client/app/app.routes.ts
@@ -9,6 +9,7 @@ import { Routes } from '@angular/router';
 //This constants holds the configuration for all the necessary routes within the application
 export const routes: Routes = [
     { path: '', component: LandingPageComponent, children: [
+        { path: '', component: LoginComponent},
         { path: 'login', component: LoginComponent },
         { path: 'signup', component: SignupComponent }
     ] },

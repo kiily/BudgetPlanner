98a19b56380cd64f2cfa35bb42dc1a0193205ab1
Miguel Marin Vermelho <miguelmarin95@hotmail.com>
2017-11-02 15:20:43 +0000

    can now update a budget name and delete a budget from the list, frontend-backend connection  finalized

--- client/app/budget-module/budget/budget.component.ts
+++ client/app/budget-module/budget/budget.component.ts
@@ -1,4 +1,5 @@
- import { Budget } from '../../models/budget.model';
+import { BudgetService } from '../../services/budget.service';
+import { Budget } from './../../models/budget.model';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
@@ -11,12 +12,31 @@ export class BudgetComponent implements OnInit {
   @Input() budget : Budget;
   @Output() budgetHoverEvt = new EventEmitter<Budget>();
 
-  constructor() { }
+  selectedBudget : Budget;
+
+  constructor(private budgetService : BudgetService) { }
 
   ngOnInit() {
   }
 
   budgetHover(budget : Budget){
     this.budgetHoverEvt.emit(budget);
+  }
+
+  selectBudget(budget : Budget){
+    //navigate to the budget detail page and pass the object in the request
+  }
+
+  onEdit(){
+    //pass the edit event to the budget service so that it can be picked up by another component
+    this.budgetService.editBudget(this.budget);
+    console.log(this.budget);
+
+  }
+
+  onDelete(){
+    this.budgetService.deleteBudget(this.budget).subscribe(
+      result => console.log(result)
+    )
   }
 }
--- client/app/services/budget.service.ts
+++ client/app/services/budget.service.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs/Observable';
 import { Budget } from './../models/budget.model';
 import { Http, Headers, Response } from '@angular/http';
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
@@ -11,6 +11,7 @@ This includes all budget CRUD operations */
 export class BudgetService {
   //keeps an internal array of the budgets
   budgets : Budget[] =[];
+  budgetIsEdit : EventEmitter<Budget> = new EventEmitter<Budget>();
 
   constructor(private http : Http) { }
 
@@ -50,14 +51,14 @@ export class BudgetService {
 
     //retrieve the token to add to the query
     const token = localStorage.getItem('token') ? '?token='+localStorage.getItem('token') : '';
-    console.log("adding budget");
+    
     //retrieve the jwt token with the user info from local storage
     return this.http.post('http://localhost:3000/budget'+token, body, {headers: headers})
     .map((response : Response) => {
       let result = response.json();
 
       //create a new budget object to add
-      let budget = new Budget(result.obj.name, result.obj.date, result.obj.user._id);
+      let budget = new Budget(result.obj.name, result.obj.date, result.obj._id, result.obj.user._id);
       this.budgets.push(budget);
       return budget;
     }).catch((error : Response) => {
@@ -67,11 +68,53 @@ export class BudgetService {
     
   }
 
+  //Emits an event when the user wants to edit a budget; use internal event emitters
+  editBudget(budget:Budget){
+    this.budgetIsEdit.emit(budget);
+    console.log('service');
+    console.log(budget);
+  }
+
+  //Send the data to update the budget on the backend
   updateBudget(budget : Budget){
+    console.log('update');
+    console.log(budget);
+    //similar code to add but using patch instead
+    const body = JSON.stringify(budget);
+    const headers = new Headers({'Content-Type':'application/json'});
+
+    //retrieve the token to add to the query
+    const token = localStorage.getItem('token') ? '?token='+localStorage.getItem('token') : '';
+    
+    //retrieve the jwt token with the user info from local storage
+    return this.http.patch('http://localhost:3000/budget/'+budget.budgetId+token, body, {headers: headers})
+    .map((response : Response) => {
+    response.json();
+    }).catch((error : Response) => {
+      //needs further error handling logic
+      return Observable.throw(error.json());
+    });
+    
 
   }
 
   deleteBudget(budget : Budget){
+    //remove from the internal array
+    this.budgets.splice(this.budgets.indexOf(budget),1);
+
+    //retrieve token
+    const token = localStorage.getItem('token') ? "?token="+localStorage.getItem('token') : "";
+
+    //pass the ID of the budget to the backend
+    return this.http.delete('http://localhost:3000/budget/'+budget.budgetId + token)
+    .map( (response : Response) => {
+      //extract response in json format
+      response.json();
+    }).catch((error : Response) => {
+      //error handling will go here
+      return Observable.throw(error.json());
+    });
     
+
   }
 }

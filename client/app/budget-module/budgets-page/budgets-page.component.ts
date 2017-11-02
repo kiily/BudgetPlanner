f0785046cd8e825184b6457b98e9f8faa86af9ee
Miguel Marin Vermelho <miguelmarin95@hotmail.com>
2017-11-02 01:50:27 +0000

    made buttons horizontal on budgets page, backend patch and delete ready

--- client/app/budget-module/budgets-page/budgets-page.component.ts
+++ client/app/budget-module/budgets-page/budgets-page.component.ts
@@ -13,19 +13,13 @@ export class BudgetsPageComponent implements OnInit {
   budgets : Budget[] = [];
   selectedBudget : Budget;
   
- 
-
-
 
   //toggles the checkbox to select the budget to delete
   checkboxToggle : boolean = false;
+  
   constructor(private budgetService : BudgetService) { }
 
   ngOnInit() {
-
-
-    
-
     //get the user's budgets upon loading
     this.budgetService.getBudgets().subscribe( (budgets : Budget[]) => {
       this.budgets = budgets;
